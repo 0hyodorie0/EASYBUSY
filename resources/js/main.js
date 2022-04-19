78ff1b0927a841171fc87797b7e87d875c18f6e2
Sohn Hyosun <48785364+0hyodorie0@users.noreply.github.com>
2022-04-19 18:00:56 +0900

    그룹웨어 공통 레이아웃

---
+++ resources/js/main.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('.gw-menu-list').on('click', function() {
+        $('.gw-menu-list').removeClass('active');
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).siblings().removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+    $('.gw-menu-item').on('click', function() {
+        $('.gw-menu-item').removeClass('active');
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).siblings().removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+});
